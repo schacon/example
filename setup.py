58321669a9153595bb50d7962d660aa102b34e41
Michael Charles <mike@Materia.local>
2015-06-08 14:12:40 -0400

    Add additional files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='example',
+      version='0.1',
+      description='Example repo',
+      url='https://github.com/mike-charles/example',
+      author='Mike Charles',
+      author_email='mike.charles@example.com',
+      license='MIT',
+      packages=['example'],
+      zip_safe=False)
